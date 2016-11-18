59e545c095cb9323937670c5c5904d0ab4c1253b
Richa Deshmukh <richa008@gmail.com>
2016-11-17 18:40:29 -0600

    Adding String problem and unit test

--- src/com/ctci/problems/ArrayStringProblems.java
+++ src/com/ctci/problems/ArrayStringProblems.java
@@ -2,8 +2,32 @@ package com.ctci.problems;
 
 /**
  * Created by richa on 11/16/16.
+ * CTCI Array and String Problems
  */
 public class ArrayStringProblems {
 
+    //TODO: Solve without using extra array
+    /**
+     * Determine if a string has unique characters
+     * Assumption made - String is made of unicode characters
+     * @param - String
+     * @return - Returns true if the string has unique characters
+     */
+    public boolean doesStringHaveUniqueCharacters(String myString){
+        if(myString.length() > 128){
+            return false;
+        }
 
+        boolean charArray[] = new boolean[256];
+        for(int i = 0; i < myString.length(); i++){
+            char c = myString.charAt(i);
+            if(charArray[c]){
+                return false;
+            }else{
+                charArray[c] = true;
+            }
+        }
+        return true;
+    }
 }
+
---
+++ src/com/ctci/tests/ArrayStringProblemsTest.java
@@ -0,0 +1,31 @@
+package com.ctci.tests;
+
+import com.ctci.problems.ArrayStringProblems;
+import static org.junit.Assert.*;
+
+/**
+ * Created by richa on 11/16/16.
+ */
+public class ArrayStringProblemsTest {
+
+    private ArrayStringProblems prob;
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        prob = new ArrayStringProblems();
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void doesStringHaveUniqueCharactersTest() throws Exception{
+        assertEquals(prob.doesStringHaveUniqueCharacters(""), true);
+        assertEquals(prob.doesStringHaveUniqueCharacters("abcdba"), false);
+        assertEquals(prob.doesStringHaveUniqueCharacters("abhjk"), true);
+
+    }
+
+}

f7ed675a8cca23202614776744059f5a0c24531f
Richa Deshmukh <richa008@gmail.com>
2016-12-17 19:04:22 -0600

    Adding three stacks problem

---
+++ src/com/java/ctci/tests/ThreeStacksUsingArrayTest.java
@@ -0,0 +1,31 @@
+package com.java.ctci.tests;
+
+import com.java.ctci.problems.stacks.ThreeStacksUsingArray;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by richa on 12/17/16.
+ *
+ */
+//TODO: Write test cases
+public class ThreeStacksUsingArrayTest {
+
+    private ThreeStacksUsingArray threeStacks = new ThreeStacksUsingArray();
+
+    @Before
+    public void setup() throws Exception{
+
+    }
+
+    @Test
+    public void push() throws Exception {
+
+    }
+
+    @Test
+    public void pop() throws Exception {
+
+    }
+
+}

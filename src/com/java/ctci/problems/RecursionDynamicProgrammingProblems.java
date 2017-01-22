79638b98288fcbae57781bcc043be295c11e0cd9
Richa Deshmukh <richa008@gmail.com>
2017-01-22 10:37:58 -0600

    Adding recursion problems

--- src/com/java/ctci/problems/RecursionDynamicProgrammingProblems.java
+++ src/com/java/ctci/problems/RecursionDynamicProgrammingProblems.java
@@ -1,5 +1,7 @@
 package com.java.ctci.problems;
 
+import java.util.ArrayList;
+
 /**
  * Created by richa on 1/15/17.
  *
@@ -101,5 +103,105 @@ public class RecursionDynamicProgrammingProblems {
         int rightIndex = Math.max(mid + 1, array[mid]);
         int right = findMagicIndex2(array, rightIndex, end);
         return right;
+    }
+
+    /**
+     * Returns all subsets of the set. Time complexity O(2^n)
+     *
+     * @param set - Set of integers
+     * @return - ArrayList of all subsets
+     */
+    public ArrayList<ArrayList<Integer>> getAllSubsets(ArrayList<Integer> set){
+
+        int n = set.size();
+        int max = 1 << n; // Computes 2^n
+
+        ArrayList<ArrayList<Integer>> allSubsets = new ArrayList<ArrayList<Integer>>();
+
+        for(int i = 0; i < max; i++){
+            ArrayList<Integer> list = convertIntToSet(set, i);
+            allSubsets.add(list);
+        }
+
+        return allSubsets;
+    }
+
+    private ArrayList<Integer> convertIntToSet(ArrayList<Integer> set, int num){
+        int index = 0;
+        ArrayList<Integer> subset = new ArrayList<Integer>();
+
+        for(int i = num; i > 0; i >>= 1){
+            if((i & 1) == 1){
+                subset.add(set.get(index));
+            }
+            index++;
+        }
+        return subset;
+    }
+
+    /**
+     * Get all permutations of string with unique characters. Time complexity O(n!)
+     * @param string - string with unique characters
+     * @return - Array list of permutations
+     */
+    public ArrayList<String> getPermutations(String string){
+
+        if(string == null){
+            return null;
+        }
+
+        if(string.length() == 0){
+            ArrayList<String> permutations = new ArrayList<String>();
+            permutations.add("");
+            return permutations;
+        }
+
+        char firstChar = string.charAt(0);
+        String remainingStr = string.substring(1); // Remove first char
+
+        ArrayList<String> permutations = getPermutations(remainingStr);
+
+        for(String permutation: permutations){
+            for(int i = 0; i <= permutation.length(); i++){
+                String s = insertCharacter(permutation, i, firstChar);
+                permutations.add(s);
+            }
+        }
+        return permutations;
+    }
+
+    private String insertCharacter(String permutation, int index, char c){
+        String start = permutation.substring(0, index);
+        String end = permutation.substring(index);
+        return  start + c + end;
+    }
+
+    /**
+     *  Print all valid combinations of n pair parenthesis
+     *  @param n
+     */
+    public void printAllValidParenthesis(int n){
+        char array[] = new char[n*2];
+        printValidParenthesisHelper(array, 0, n, n);
+    }
+
+    public void printValidParenthesisHelper(char array[], int count, int leftRemaining, int rightRemaining){
+        if(leftRemaining < 0 || rightRemaining < leftRemaining){
+            return;
+        }
+
+        if(leftRemaining == 0 && rightRemaining == 0){
+            System.out.println(array);
+        }
+
+        if(leftRemaining > 0){
+            array[count] = '(';
+            printValidParenthesisHelper(array, count+1, leftRemaining -1 , rightRemaining);
+        }
+
+        if(rightRemaining > leftRemaining){
+            array[count] = ')';
+            printValidParenthesisHelper(array, count + 1, leftRemaining, rightRemaining - 1);
+        }
     }
 }
